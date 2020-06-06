ba050834e4b8033d5db3737b773a679d90b3fea0
James Gowans <jgowans@amazon.com>
2020-06-06 23:14:41 +0200

    Add userfaultfd registering and handling
    Now the sender thread sets up userfaultfd on an VMA, then sends the
    userfaultfd fd to the reciever, which blocks on it and handles the
    faults.

--- receiver.c
+++ receiver.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -6,7 +7,14 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <poll.h>
+#include <linux/userfaultfd.h>
+#include <string.h>
+#include <sys/ioctl.h>
 #include "shared-header.h"
+
+#define errExit(msg) do { perror(msg); exit(EXIT_FAILURE); \
+		        } while (0)
 
 static int extract_fd(struct msghdr *msgh) {
 	struct cmsghdr *cmsg = CMSG_FIRSTHDR(msgh);
@@ -43,12 +51,99 @@ static int read_fd_from_socket(int socket_fd) {
 	return extract_fd(&msg);
 }
 
+void handle_userfault(int uffd) {
+	static struct uffd_msg msg;   /* Data read from userfaultfd */
+	static int fault_cnt = 0;     /* Number of faults so far handled */
+	static char *page = NULL;
+	struct uffdio_copy uffdio_copy;
+	ssize_t nread;
+	int page_size = sysconf(_SC_PAGE_SIZE);
+
+	/* Create a page that will be copied into the faulting region */
+
+	if (page == NULL) {
+		page = mmap(NULL, page_size, PROT_READ | PROT_WRITE,
+				MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+		if (page == MAP_FAILED)
+			errExit("mmap");
+	}
+
+	/* Loop, handling incoming events on the userfaultfd
+	   file descriptor */
+
+	for (;;) {
+
+		/* See what poll() tells us about the userfaultfd */
+
+		struct pollfd pollfd;
+		int nready;
+		pollfd.fd = uffd;
+		pollfd.events = POLLIN;
+		nready = poll(&pollfd, 1, -1);
+		if (nready == -1)
+			errExit("poll");
+
+		printf("\nfault_handler_thread():\n");
+		printf("    poll() returns: nready = %d; "
+				"POLLIN = %d; POLLERR = %d\n", nready,
+				(pollfd.revents & POLLIN) != 0,
+				(pollfd.revents & POLLERR) != 0);
+
+		/* Read an event from the userfaultfd */
+
+		nread = read(uffd, &msg, sizeof(msg));
+		if (nread == 0) {
+			printf("EOF on userfaultfd!\n");
+			exit(EXIT_FAILURE);
+		}
+
+		if (nread == -1)
+			errExit("read");
+
+		/* We expect only one kind of event; verify that assumption */
+
+		if (msg.event != UFFD_EVENT_PAGEFAULT) {
+			fprintf(stderr, "Unexpected event on userfaultfd\n");
+			exit(EXIT_FAILURE);
+		}
+
+		/* Display info about the page-fault event */
+
+		printf("    UFFD_EVENT_PAGEFAULT event: ");
+		printf("flags = %llx; ", msg.arg.pagefault.flags);
+		printf("address = %llx\n", msg.arg.pagefault.address);
+
+		/* Copy the page pointed to by 'page' into the faulting
+		   region. Vary the contents that are copied in, so that it
+		   is more obvious that each fault is handled separately. */
+
+		memset(page, 'A' + fault_cnt % 20, page_size);
+		fault_cnt++;
+
+		uffdio_copy.src = (unsigned long) page;
+
+		/* We need to handle page faults in units of pages(!).
+		   So, round faulting address down to page boundary */
+
+		uffdio_copy.dst = (unsigned long) msg.arg.pagefault.address &
+			~(page_size - 1);
+		uffdio_copy.len = page_size;
+		uffdio_copy.mode = 0;
+		uffdio_copy.copy = 0;
+		if (ioctl(uffd, UFFDIO_COPY, &uffdio_copy) == -1)
+			errExit("ioctl-UFFDIO_COPY");
+
+		printf("        (uffdio_copy.copy returned %lld)\n",
+				uffdio_copy.copy);
+	}
+}
+
 int main(int argc, char **argv) {
+	int uffd;
 
 	printf("In receiver pid %i\n", getpid());
-	for(;;) {
-		printf("Receiver got fd number: %i\n",
-				read_fd_from_socket(4));
-	}
+	uffd = read_fd_from_socket(4);
+	printf("Receiver got uffd: %i\n", uffd);
+	handle_userfault(uffd);
 	return 0;
 }
--- sender.c
+++ sender.c
@@ -7,7 +7,13 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <string.h>
+#include <linux/userfaultfd.h>
+#include <syscall.h>
+#include <sys/ioctl.h>
 #include "shared-header.h"
+
+#define errExit(msg) do { perror(msg); exit(EXIT_FAILURE); \
+		        } while (0)
 
 static int send_fd_over_socket(int socket_fd, int fd_to_send) {
 	struct iovec io;
@@ -39,16 +45,59 @@ static int send_fd_over_socket(int socket_fd, int fd_to_send) {
 	return sendmsg(socket_fd, &msg, 0);
 }
 
+static void register_remote_userfaultfd(void *addr, unsigned long len) {
+	int uffd;
+	struct uffdio_api uffdio_api;
+	struct uffdio_register uffdio_register;
+
+	uffd = syscall(__NR_userfaultfd, O_CLOEXEC | O_NONBLOCK);
+	if (uffd == -1)
+		errExit("userfaultfd");
+
+	uffdio_api.api = UFFD_API;
+	uffdio_api.features = 0;
+	if (ioctl(uffd, UFFDIO_API, &uffdio_api) == -1)
+		errExit("ioctl-UFFDIO_API");
+
+	uffdio_register.range.start = (unsigned long) addr;
+	uffdio_register.range.len = len;
+	uffdio_register.mode = UFFDIO_REGISTER_MODE_MISSING;
+	if (ioctl(uffd, UFFDIO_REGISTER, &uffdio_register) == -1)
+		errExit("ioctl-UFFDIO_REGISTER");
+
+
+	if (send_fd_over_socket(3, uffd) < 0)
+		printf("Failed to send message\n");
+}
+
 int main(int argc, char **argv) {
-	int fd;
+	char *addr;         /* Start of region handled by userfaultfd */
+	unsigned long len;  /* Length of region handled by userfaultfd */
 
 	printf("In sender pid %i\n", getpid());
-	fd = open("/dev/zero", O_RDWR);
 
-	for(;;) {
-		if (send_fd_over_socket(3, fd) < 0)
-			printf("Failed to send message\n");
-		sleep(1);
+	len = sysconf(_SC_PAGE_SIZE) * 4;
+
+	addr = mmap(NULL, len, PROT_READ | PROT_WRITE,
+			MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+	if (addr == MAP_FAILED)
+		errExit("mmap");
+
+	printf("Address returned by mmap() = %p\n", addr);
+
+
+	register_remote_userfaultfd(addr, len);
+
+	int l;
+	l = 0xf;    /* Ensure that faulting address is not on a page
+		       boundary, in order to test that we correctly
+		       handle that case in fault_handling_thread() */
+	while (l < len) {
+		char c = addr[l];
+		printf("Read address %p in main(): ", addr + l);
+		printf("%c\n", c);
+		l += 1024;
+		usleep(100000);         /* Slow things down a little */
 	}
 	return 0;
 }
